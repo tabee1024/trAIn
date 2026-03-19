3ac51c38347d9246e5f94749fe7083143aef6212
ReenuMohan2022 <112227549+ReenuMohan2022@users.noreply.github.com>
2026-03-19 15:38:17 -0700

    Updated Search.kt
    Signed-off-by: ReenuMohan2022 <112227549+ReenuMohan2022@users.noreply.github.com>

--- TrAInApp/app/src/main/java/com/example/trainapp/Search.kt
+++ TrAInApp/app/src/main/java/com/example/trainapp/Search.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -15,11 +13,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.trainapp.ui.theme.Swirl
-import com.example.trainapp.ui.theme.Toast
+import com.example.trainapp.ui.theme.DBrown
 import com.example.trainapp.ui.theme.TrAInAppTheme
 
 @Composable
@@ -27,23 +25,12 @@ fun Search() {
     Box(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier
             .fillMaxSize()
-            .background(color = Swirl)
+            .background(color = Color.White)
             .padding(16.dp)
-            .verticalScroll(rememberScrollState())
-            .align(Alignment.TopStart),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start) {
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .background(color = Toast)
-                .padding(8.dp),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = "Search", style = MaterialTheme.typography.titleSmall, fontSize = 30.sp, color = Swirl)
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-
+            .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Loading ...", style = MaterialTheme.typography.titleSmall, fontSize = 30.sp, color = DBrown)
         }
     }
 }
