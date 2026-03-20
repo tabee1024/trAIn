59951adbf5c63b8c1ff685076a8f54f3e774d219
ReenuMohan2022 <112227549+ReenuMohan2022@users.noreply.github.com>
2026-03-20 14:18:49 -0700

    Updated Notifications.kt
    Signed-off-by: ReenuMohan2022 <112227549+ReenuMohan2022@users.noreply.github.com>

--- TrAInApp/app/src/main/java/com/example/trainapp/Notifications.kt
+++ TrAInApp/app/src/main/java/com/example/trainapp/Notifications.kt
@@ -1,32 +1,35 @@
 package com.example.trainapp
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.trainapp.ui.theme.Emperor
+import com.example.trainapp.ui.theme.DBrown
 import com.example.trainapp.ui.theme.TrAInAppTheme
 
 @Composable
 fun Notifications() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .align(Alignment.Center),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = "Notifications", fontSize = 30.sp, color = Emperor)
-        }
+    Column(modifier = Modifier.fillMaxSize()
+        .background(color = Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Notifications", fontSize = 30.sp, color = DBrown)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Loading...", fontSize = 12.sp, color = DBrown)
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun NotificationsPreview() {
     TrAInAppTheme {
