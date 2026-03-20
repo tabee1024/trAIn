41547e343eb14eda7db03e5b601d84c575d5d43c
ReenuMohan2022 <112227549+ReenuMohan2022@users.noreply.github.com>
2026-03-20 14:22:21 -0700

    Update Screens.kt
    Signed-off-by: ReenuMohan2022 <112227549+ReenuMohan2022@users.noreply.github.com>

--- TrAInApp/app/src/main/java/com/example/trainapp/Screens.kt
+++ TrAInApp/app/src/main/java/com/example/trainapp/Screens.kt
@@ -1,6 +1,5 @@
 package com.example.trainapp
 
-
 sealed class Screens (val screen: String) {
     data object Home: Screens("home")
     data object Profile: Screens("profile")
@@ -13,12 +12,8 @@ sealed class Screens (val screen: String) {
     data object Settings: Screens("settings")
     data object Search: Screens("search")
     data object Progress: Screens("progress")
-
     object WorkoutList : Screens("workout_list")
-
     object PushUp : Screens("push_up")
-
     object Squats : Screens("squats")
-
     object Lunges : Screens("lunges")
 }
