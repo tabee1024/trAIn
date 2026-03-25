590d4a12d4f2d0f8b5f6008693d57ca43ec715e1
ReenuMohan2022 <112227549+ReenuMohan2022@users.noreply.github.com>
2026-03-24 20:14:03 -0700

    Updated color theme
    Signed-off-by: ReenuMohan2022 <112227549+ReenuMohan2022@users.noreply.github.com>

--- TrAInApp/app/src/main/java/com/example/trainapp/AboutUs.kt
+++ TrAInApp/app/src/main/java/com/example/trainapp/AboutUs.kt
@@ -21,15 +21,14 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.trainapp.ui.theme.Brown
-import com.example.trainapp.ui.theme.DBrown
+import com.example.trainapp.ui.theme.Emperor
 import com.example.trainapp.ui.theme.TrAInAppTheme
 
 @Composable
 fun AboutUs() {
     Column(modifier = Modifier
         .fillMaxSize()
-        .background(color = Beige)
+        .background(color = Color.White)
         .padding(16.dp)
         .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Top,
@@ -41,7 +40,7 @@ fun AboutUs() {
             style = MaterialTheme.typography.titleMedium,
             fontWeight = FontWeight.SemiBold,
             fontSize = 30.sp,
-            color = DBrown
+            color = Color.Black
         )
 
         // Details
@@ -57,42 +56,42 @@ fun AboutContents() {
         .padding(16.dp)
     ) {
         // Mission Statement
-        Text(text = "Our Mission", fontWeight = FontWeight.Bold, style = MaterialTheme.typography.headlineSmall, color = DBrown)
-        HorizontalDivider(modifier = Modifier.fillMaxWidth(), color = Brown, thickness = 0.5.dp)
+        Text(text = "Our Mission", fontWeight = FontWeight.Bold, style = MaterialTheme.typography.headlineSmall, color = Color.Black)
+        HorizontalDivider(modifier = Modifier.fillMaxWidth(), color = Emperor, thickness = 0.5.dp)
         Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = "We aim to empower users with engaging fitness tools, personalized guidance, and accessible resources to help them achieve their health goals.",
-            color = DBrown, style = MaterialTheme.typography.bodyLarge
+            color = Color.Black, style = MaterialTheme.typography.bodyLarge
         )
         Spacer(modifier = Modifier.height(16.dp))
 
         //Major Features
-        Text(text = "Major Features", fontWeight = FontWeight.Bold, style = MaterialTheme.typography.headlineSmall, color = DBrown)
-        HorizontalDivider(modifier = Modifier.fillMaxWidth(), color = Brown, thickness = 0.5.dp)
+        Text(text = "Major Features", fontWeight = FontWeight.Bold, style = MaterialTheme.typography.headlineSmall, color = Color.Black)
+        HorizontalDivider(modifier = Modifier.fillMaxWidth(), color = Emperor, thickness = 0.5.dp)
         Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = "• Interactive workout plans\n• Progress tracking dashboard\n• Favorites for quick access\n• Time management tools\n• Helpful guides and tips",
-            color = DBrown, style = MaterialTheme.typography.bodyLarge
+            color = Color.Black, style = MaterialTheme.typography.bodyLarge
         )
         Spacer(modifier = Modifier.height(16.dp))
 
         // Our Audience
-        Text(text = "Our Audience",fontWeight = FontWeight.Bold, style = MaterialTheme.typography.headlineSmall, color = DBrown)
-        HorizontalDivider(modifier = Modifier.fillMaxWidth(), color = Brown, thickness = 0.5.dp)
+        Text(text = "Our Audience",fontWeight = FontWeight.Bold, style = MaterialTheme.typography.headlineSmall, color = Color.Black)
+        HorizontalDivider(modifier = Modifier.fillMaxWidth(), color = Emperor, thickness = 0.5.dp)
         Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = "We serve fitness enthusiasts, beginners starting their journey, and anyone looking for structured, motivating tools to improve their lifestyle.",
-            color = DBrown, style = MaterialTheme.typography.bodyLarge
+            color = Color.Black, style = MaterialTheme.typography.bodyLarge
         )
         Spacer(modifier = Modifier.height(16.dp))
 
         //Disclaimer
-        Text(text = "Disclaimer", fontWeight = FontWeight.Bold, style = MaterialTheme.typography.headlineSmall, color = DBrown)
-        HorizontalDivider(modifier = Modifier.fillMaxWidth(), color = Brown, thickness = 0.5.dp)
+        Text(text = "Disclaimer", fontWeight = FontWeight.Bold, style = MaterialTheme.typography.headlineSmall, color = Color.Black)
+        HorizontalDivider(modifier = Modifier.fillMaxWidth(), color = Emperor, thickness = 0.5.dp)
         Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = "This app is intended for informational and motivational purposes only. It is not a substitute for professional medical advice. Always consult a healthcare provider before starting any new fitness program.",
-            color = DBrown, style = MaterialTheme.typography.bodyLarge
+            color = Color.Black, style = MaterialTheme.typography.bodyLarge
         )
         Spacer(modifier = Modifier.height(16.dp))
     }
